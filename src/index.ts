d37922ab0bc6aced477c4379c6d3899fae094efb
Amoong <moo1323@naver.com>
2023-09-11 17:25:42 +0900

    Serverless WebSockets

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,12 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+//@ts-ignore
+import home from './home.html';
 
 export interface Env {
 	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
 	// MY_KV_NAMESPACE: KVNamespace;
 	//
 	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	COUNTER: DurableObjectNamespace;
+	CHAT: DurableObjectNamespace;
 	//
 	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
 	// MY_BUCKET: R2Bucket;
@@ -25,10 +18,49 @@ export interface Env {
 	// MY_QUEUE: Queue;
 }
 
-export class CounterObject {
-	counter: number;
-	constructor() {
-		this.counter = 0;
+export class ChatRoom {
+	state: DurableObjectState;
+	constructor(state: DurableObjectState, env: Env) {
+		this.state = state;
+	}
+
+	async getCounter() {
+		const counter: string | undefined = await this.state.storage.get('counter');
+
+		if (!counter) {
+			return 0;
+		} else {
+			return parseInt(counter);
+		}
+	}
+
+	handleHome() {
+		return new Response(home, {
+			headers: {
+				'Content-type': 'text/html',
+			},
+		});
+	}
+
+	handleConnect(request: Request) {
+		const pairs = new WebSocketPair();
+
+		this.handleWebSocket(pairs[1]);
+
+		return new Response(null, { status: 101, webSocket: pairs[0] });
+	}
+
+	handleWebSocket(webSocket: WebSocket) {
+		webSocket.accept();
+		setTimeout(() => {
+			webSocket.send(JSON.stringify({ message: 'hello from backend!' }));
+		}, 3000);
+	}
+
+	handleNotFound() {
+		return new Response(null, {
+			status: 404,
+		});
 	}
 
 	async fetch(request: Request) {
@@ -36,25 +68,20 @@ export class CounterObject {
 
 		switch (pathname) {
 			case '/':
-				return new Response(this.counter.toString());
-			case '/+':
-				this.counter++;
-				return new Response(this.counter.toString());
-			case '/-':
-				this.counter--;
-				return new Response(this.counter.toString());
+				return this.handleHome();
+			case '/connect':
+				return this.handleConnect(request);
+
 			default:
-				return new Response(null, {
-					status: 404,
-				});
+				return this.handleNotFound();
 		}
 	}
 }
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		const id = env.COUNTER.idFromName('counter');
-		const durableObject = env.COUNTER.get(id);
+		const id = env.CHAT.idFromName('counter');
+		const durableObject = env.CHAT.get(id);
 		const response = await durableObject.fetch(request);
 		return response;
 	},
